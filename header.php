d3be4313719d9e63fc10add50f53507188e48816
do0k <do0k@outlook.com>
2018-07-05 12:48:15 +0430

    second commit

--- header.php
+++ header.php
@@ -14,8 +14,8 @@
 <header>
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
 		<a class="navbar-brand ml-3" href="<?php bloginfo('url') ?>">
-			<img src="<?php bloginfo('template_url') ?>/assets/img/pw-logo.png" class="d-inline-block align-middle ml-2" alt="logo">
-			<h1 class=""><?php bloginfo('title') ?></h1>
+			<img src="<?php bloginfo('template_url') ?>/assets/img/pw-logo-sharp.png" class="d-inline-block align-middle ml-2" alt="logo">
+			<h1 class="d-inline-block pw-logo-sharp"><?php bloginfo('title') ?></h1>
 		</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
